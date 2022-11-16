5c1ec221d6aa8971e96ec3f7bdd160223c75bf07
Emil Olsen <emiols2003@skola.goteborg.se>
2022-11-16 11:45:25 +0100

    Changed the allowed guesses to be from 0 to 10

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,6 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        var guesser = new Guesser(0, 1000);
+        var guesser = new Guesser(0, 10);
         guesser.start();
     }
 }
